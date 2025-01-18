211897c2755bfcd24323702ea45e29ec22b7ae7d
Tyler Albert <tylerrexford@gmail.com>
2025-01-17 23:46:25 -0500

    Add primitive inventory system
    Creates Resource files for managing the player's inventory.
    Updates the farm's `AddJellyButton` to add jellies to the inventory and save it.
    Adds BlueJelly as baseline jelly type.
    
    Temp.

--- praccyproggy/farm.gd
+++ praccyproggy/farm.gd
@@ -2,20 +2,21 @@ extends Node2D
 @export var jellyTemplate: PackedScene
 
 
-
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	
-	pass # Replace with function body
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
-	pass
-
+	var inventory: Inventory = Inventory.load()
+	for jelly_instance in inventory.owned_jellies:
+		spawn_jelly(jelly_instance)
 
 func _on_add_jelly_button_pressed() -> void:
+	var inventory: Inventory = Inventory.load()
+	var new_jelly_instance = JellyInstance.new("New name")
+	inventory.owned_jellies.append(new_jelly_instance)
+	inventory.save()
+	spawn_jelly(new_jelly_instance)
+
+func spawn_jelly(jelly_instance: JellyInstance) -> void:
 	var jelly = jellyTemplate.instantiate()
+	# TODO: tyleralbert - Populate jelly with data from jelly_instance.
 	jelly.position = $JellySpawner.position
 	add_child(jelly)
-	##jelly.global_position = $jellySpawn
-	print("buttonpressed")
