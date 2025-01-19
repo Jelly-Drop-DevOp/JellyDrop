1e01044ef72600d339ac451362e4ef1f606e3e30
Tyler Albert <tylerrexford@gmail.com>
2025-01-19 17:04:20 -0500

    Add basic mixer menu back by Inventory.

--- praccyproggy/mixer/mixer.gd
+++ praccyproggy/mixer/mixer.gd
@@ -1,12 +1,20 @@
 extends Node2D
 
+@export var jelly_list_entry_template: PackedScene
+@export var jelly_entry_markers: Array[Marker2D]
+@export var left_cup: Marker2D
+@export var right_cup: Marker2D
+@export var result: Marker2D
+
 func _ready() -> void:
 	var inventory: Inventory = Inventory.load()
 	for jelly_instance in inventory.owned_jellies:
 		add_jelly_list_entry(jelly_instance)
 
 func add_jelly_list_entry(jelly_instance: BlueJelly) -> void:
-	#TODO: tyleralbert - Instantiate custom UI element with `jelly_instance` fields.
-	var button: Button = Button.new()
-	button.text = "Test"
-	$JellyListContainer/JellyList.add_child(button)
+	var inventory: Inventory = Inventory.load()
+	for i in range(0, min(jelly_entry_markers.size(), inventory.owned_jellies.size())):
+		# TODO: tyleralbert - Populate jelly with data from jelly_instance.
+		var jelly_list_entry = jelly_list_entry_template.instantiate()
+		jelly_list_entry.position = jelly_entry_markers[i].position
+		add_child(jelly_list_entry)
