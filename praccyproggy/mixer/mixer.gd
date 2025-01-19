0b75a3a572d59d5e73eb0fa072207b0b148eb929
Tyler Albert <tylerrexford@gmail.com>
2025-01-19 15:10:42 -0500

    Fix updating of /mixer files.

--- praccyproggy/mixer/mixer.gd
+++ praccyproggy/mixer/mixer.gd
@@ -1,5 +1,4 @@
 extends Node2D
-@export var mixer_list_entry_template: PackedScene
 
 func _ready() -> void:
 	var inventory: Inventory = Inventory.load()
@@ -7,7 +6,7 @@ func _ready() -> void:
 		add_jelly_list_entry(jelly_instance)
 
 func add_jelly_list_entry(jelly_instance: BlueJelly) -> void:
-	var mixer_list_entry = mixer_list_entry_template.instantiate()
+	#TODO: tyleralbert - Instantiate custom UI element with `jelly_instance` fields.
 	var button: Button = Button.new()
 	button.text = "Test"
 	$JellyListContainer/JellyList.add_child(button)
