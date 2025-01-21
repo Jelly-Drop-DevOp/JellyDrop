0e62feda163d721b781c589c03771b8da9697523
Tyler Albert <tylerrexford@gmail.com>
2025-01-21 00:58:30 -0500

    Add mixer cup UI
    Clicking on cups will activate them. Clicking on a jelly will make a copy of the UI element and place it in the cup.

--- praccyproggy/mixer/mixer.gd
+++ praccyproggy/mixer/mixer.gd
@@ -1,20 +1,80 @@
-extends Node2D
+class_name Mixer extends Node2D
 
-@export var jelly_list_entry_template: PackedScene
+@export var mixer_jelly_tile_template: PackedScene
 @export var jelly_entry_markers: Array[Marker2D]
-@export var left_cup: Marker2D
-@export var right_cup: Marker2D
+
+@export var left_cup: Node2D
+var left_cup_selected: bool = false
+@export var right_cup: Node2D
+var right_cup_selected: bool = false
+
 @export var result: Marker2D
+
+var selected_tile: Node2D = null
 
 func _ready() -> void:
 	var inventory: Inventory = Inventory.load()
 	for jelly_instance in inventory.owned_jellies:
 		add_jelly_list_entry(jelly_instance)
+	left_cup.get_node("CharacterBody2D").connect("input_event", self._on_left_cup_input)
+	right_cup.get_node("CharacterBody2D").connect("input_event", self._on_right_cup_input)
 
 func add_jelly_list_entry(jelly_instance: BlueJelly) -> void:
-	var inventory: Inventory = Inventory.load()
-	for i in range(0, min(jelly_entry_markers.size(), inventory.owned_jellies.size())):
-		# TODO: tyleralbert - Populate jelly with data from jelly_instance.
-		var jelly_list_entry = jelly_list_entry_template.instantiate()
-		jelly_list_entry.position = jelly_entry_markers[i].position
-		add_child(jelly_list_entry)
+	for marker in jelly_entry_markers:
+		if marker.get_child_count() > 0:
+			continue
+
+		var tile: Node2D = mixer_jelly_tile_template.instantiate()
+		marker.add_child(tile)
+		var body: CharacterBody2D = tile.get_node("CharacterBody2D")
+		body.connect("input_event", self._on_jelly_list_entry_input.bind(tile, jelly_instance))
+		return
+
+	print("Unable to populate jelly list entry for jelly: " + jelly_instance.name)
+
+func _on_left_cup_input(viewport: Viewport, event: InputEvent, shape_idx: int):
+	if !event.is_pressed():
+		return
+
+	if left_cup_selected:
+		left_cup.scale = Vector2(1, 1)
+	else:
+		left_cup.scale = Vector2(0.8, 0.8)
+		
+	var node = left_cup.get_node("MixerJellyTile")
+	if node != null:
+		left_cup.remove_child(node)
+		
+	left_cup_selected = !left_cup_selected
+	
+func _on_right_cup_input(viewport: Viewport, event: InputEvent, shape_idx: int):
+	if !event.is_pressed():
+		return
+
+	if right_cup_selected:
+		right_cup.scale = Vector2(1, 1)
+	else:
+		right_cup.scale = Vector2(0.8, 0.8)
+	var node = right_cup.get_node("MixerJellyTile")
+	if node != null:
+		right_cup.remove_child(node)
+		
+	right_cup_selected = !right_cup_selected
+
+func _on_jelly_list_entry_input(viewport: Viewport, event: InputEvent, shape_idx: int, clicked_tile: Node2D, jelly_instance: BlueJelly):
+	if !event.is_pressed():
+		return
+
+	if right_cup_selected:
+		var tile: Node2D = mixer_jelly_tile_template.instantiate()
+		right_cup.add_child(tile)
+		right_cup.scale = Vector2(1, 1)
+		right_cup_selected = false
+		return	
+	
+	if left_cup_selected:
+		var tile: Node2D = mixer_jelly_tile_template.instantiate()
+		left_cup.add_child(tile)
+		left_cup.scale = Vector2(1, 1)
+		left_cup_selected = false
+		return
