a275664449fbe027b5c03fb2a2d70f2d203c1c96
CrystalJams <jcriz.jr@gmail.com>
2025-01-27 21:05:48 -0500

    Adds drag and drop functionality for the jellies currently allows all jellies to be picked up anytime Changes ready code for jellies to avoid global positioning problems with spawnpoints

--- praccyproggy/jelly_spawnpositions.gd
+++ praccyproggy/jelly_spawnpositions.gd
@@ -4,12 +4,6 @@ var spawncount
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	spawncount = 0
-	draw_circle(self.position, 100.0, Color.BLUE_VIOLET, true)
-	draw_circle($jellySpawn1.position, 100.0, Color.BLUE_VIOLET, true)
-	print("spawnpoint position ", self.position)
-	print("spawnpoint position ", $jellySpawn1.position)
-	print("spawnpoint position ", $jellySpawn1.global_position)
-	pass # Replace with function body.
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
@@ -21,7 +15,7 @@ func next_spawnpoint() -> Vector2:
 	print("spawncount ", spawncount)
 	print("modulo ", spawncount % 4)
 	if(spawncount % 4 == 0):
-		return self.position
+		return $jellySpawn4.global_position
 	elif(spawncount % 4 == 1):
 		return $jellySpawn1.global_position
 	elif(spawncount % 4 == 2):
